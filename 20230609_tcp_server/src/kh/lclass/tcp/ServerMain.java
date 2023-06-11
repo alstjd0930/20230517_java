303302dd7b70a4aaaddb8752d51a57d5e68617f9
alstjd0930 <alstjd0930@naver.com>
2023-06-11 18:04:23 +0900

    주석추가

--- 20230609_tcp_server/src/kh/lclass/tcp/ServerMain.java
+++ 20230609_tcp_server/src/kh/lclass/tcp/ServerMain.java
@@ -3,7 +3,7 @@ package kh.lclass.tcp;
 public class ServerMain {
 
 	public static void main(String[] args) {
-		new TcpServer().testTcpServer(9001);	
+		new TcpServer().testTcpServer(9001);	//메인서버는 포트번호,클라이언트서버는 포트번호+ip
 		
 	}
 }
--- 20230609_tcp_server/src/kh/lclass/tcp/TcpServer.java
+++ 20230609_tcp_server/src/kh/lclass/tcp/TcpServer.java
@@ -13,31 +13,31 @@ import java.net.Socket;
 public class TcpServer {
 	public void testTcpServer(int port) {
 			//1번
-		ServerSocket ss = null;
-		Socket sc = null;
-		InputStream in= null;
+		ServerSocket ss = null;		//서버 생성을 위한  서버소켓
+		Socket sc = null;			//client와 통신하기위한 소켓
+		InputStream in= null;		
 		OutputStream out = null;
-		BufferedReader br = null;
-		BufferedWriter wr = null;
+		BufferedReader br = null;	//client의 데이터를 읽어들이기위한 입력스트림
+		BufferedWriter wr = null;	//데이터를 내보내기 위한 출력 스트림
 		
 		try {
 			// 2. 서버용 소켓 객체 생성
-			ss = new ServerSocket(port);
+			ss = new ServerSocket(port); 	//ss라는 서버소켓 객체생성
 			
-		while(true) {
+		while(true) {		//반복문 
 			System.out.println("접속대기중");
 			//3. 클라이언트 쪽에서 접속 요청이 오길 기다림 + 4번 포함
-			 sc =ss.accept();
+			 sc =ss.accept();	//sc 소켓을 ss로 선언한 객체에 대기 상태
 			System.out.println("접속됨"+sc.getLocalPort());
 			System.out.println("접속됨"+sc.getPort());
 			//5번
-			 in= sc.getInputStream();
+			 in= sc.getInputStream();		//클라이언트와 연결된 입출력스트림 생성
 			 out=sc.getOutputStream();
 			//6번
-			 br = new BufferedReader(new InputStreamReader(in));
+			 br = new BufferedReader(new InputStreamReader(in));	//보조스트림도 생성
 			 wr = new BufferedWriter(new OutputStreamWriter(out));
 			
-			 String receivedMsg;
+			 String receivedMsg;	//
 			 while((receivedMsg = br.readLine()) !=null){
 				 System.out.println("받은메세지"+receivedMsg);
 				 wr.write("메세지 잘받음\n");
